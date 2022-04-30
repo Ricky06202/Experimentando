7d81c04f962a9fc4d384125f2dbfe6f0a11e3690
Ricardo A. Sanjur G <ricardosanjurg@gmail.com>
2022-04-30 15:57:24 -0500

    practicamos lo que es el encapsulamiento y creamos objetos de subclases

--- src/Experimentos/Encapsulamiento.java
+++ src/Experimentos/Encapsulamiento.java
@@ -1,7 +1,54 @@
 package Experimentos;
 
 public class Encapsulamiento {
+    private int edad;
+    // private int hora;
+    // private String nombre;
+    // private String cumpleaños;
+    public int getEdad() {
+        return edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
     public static void main(String[] args) {
-        
+        Encapsulamiento a = new Encapsulamiento();
+        Encapsulamiento.practica b = a.new practica();
+        Encapsulamiento.practica.practiquita c = b.new practiquita();
+        c.new practicasa();
+    }
+    
+    class practica{
+        practica(){
+            var e = new Encapsulamiento();
+            e.setEdad(22);
+            int a = e.getEdad();
+            System.out.println(a);
+        }
+        class practiquita{
+            practiquita(){
+                var e = new Encapsulamiento();
+                e.setEdad(22);
+                int a = e.getEdad();
+                System.out.println(a);
+            }
+            class practicasa{
+                practicasa(){
+                    var e = new Encapsulamiento();
+                    e.setEdad(22);
+                    int a = e.getEdad();
+                    System.out.println(a);
+                }
+            }
+        }
+    }
+}
+
+class hola{
+    hola(){
+        var e = new Encapsulamiento();
+        e.setEdad(22);
+        int a = e.getEdad();
+        System.out.println(a);
     }
 }
